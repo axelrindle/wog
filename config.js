d96133e43613cc54c33044d8b21f0a7dbd78f24b
Axel Rindle <axel.rindle@gmx.de>
2018-04-21 12:06:12 +0200

    Add note on file amount

--- config.js
+++ config.js
@@ -11,6 +11,9 @@ module.exports = {
   /**
    * An array of globs for selecting log files to view in the interface.
    *
+   * Note that the more files you include in your glob, the longer it will take for the
+   * application to start.
+   *
    * Read more here: https://www.npmjs.com/package/glob
    */
   logs: [
