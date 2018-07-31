c90ef2a60225ec32641e2d866802cc78675747dc
Axel Rindle <axel.rindle@gmx.de>
2018-07-31 12:18:46 +0200

    Remove custom parentDir function and simplify with path module

--- backend/util.js
+++ backend/util.js
@@ -2,15 +2,10 @@
 const path = require('path');
 
 /**
- * @returns The parent directory of a file.
- */
-const parentDir = module.exports.parentDir = file => path.dirname(file).split(path.sep).pop();
-
-/**
  * Transforms a path to show the filename and the parent directory.
  */
 module.exports.transformFilePath = file => {
-  const parent = parentDir(file);
+  const parent = path.basename(path.dirname(file));
   const base = path.basename(file);
   return path.join(parent, base);
 };
