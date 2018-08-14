9fd84244c282ac49adc478c625454bc8664c5803
Axel Rindle <axel.rindle@gmx.de>
2018-08-14 20:01:59 +0200

    Remove unused function

--- backend/util.js
+++ backend/util.js
@@ -50,18 +50,3 @@ module.exports.loadLogFiles = logs => {
     }))
   }));
 };
-
-/**
- * A sorting functions which compares the first char of two strings.
- */
-module.exports.fileSorter = (a, b) => {
-  a = a.path.charAt(0);
-  b = b.path.charAt(0);
-  if (a < b) {
-    return -1;
-  }
-  if (a > b) {
-    return 1;
-  }
-  return 0;
-};
