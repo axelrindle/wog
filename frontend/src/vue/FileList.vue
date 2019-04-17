a1a7ea79a8541795678bf7700dcd51ca4f9904a4
Axel Rindle <axel.rindle@gmx.de>
2019-04-18 01:03:39 +0200

    Create vue component for handling adapter/file selection

---
+++ frontend/src/vue/FileList.vue
@@ -0,0 +1,110 @@
+<template lang="pug">
+  div
+    // filter
+    section.section
+      .container
+        p.title Search
+        p.subtitle Find a logfile
+        .field.has-addons
+          .control
+            input.input(type='text', placeholder='boot.log' v-model="filter")
+          .control
+            a.button.is-danger(@click="filter = ''")
+              span.icon
+                i.fas.fa-trash-alt
+
+    hr
+
+    // file tree
+    section.section
+      .container
+        p.title Logfiles
+          a(v-on:click="refresh()" v-if="selected.adapter").button.is-primary.is-rounded.is-pulled-right
+            span.icon
+              i.fas.fa-sync-alt
+
+        p(v-if="allShown").subtitle Loaded {{ shown }} files
+        p(v-else).subtitle Showing {{ shown }} / {{ files.length }} files
+        .select
+          select(v-model="selected.adapter" :disabled="loading")
+            option(v-for="adapter in adapters" :value="adapter") {{ adapter }}
+        hr
+        ul#filelist
+          li(v-for="(file, index) in filesFiltered")
+            a(@click="select(index)" :title="file.path" :class="{ 'is-link': selected.file === index }").button {{ file.name }}
+</template>
+
+<script>
+module.exports = {
+  name: 'FileList',
+
+  data() {
+    return {
+      loading: true,
+      adapters: [],
+      files: [],
+      filter: '',
+      selected: {
+        adapter: null,
+        file: -1
+      },
+      error: null
+    };
+  },
+  computed: {
+    filesFiltered() {
+      return this.files.filter(el => el.name.indexOf(this.filter) !== -1);
+    },
+    shown() {
+      return this.filesFiltered.length;
+    },
+    allShown() {
+      return this.shown === this.files.length;
+    }
+  },
+  methods: {
+    refresh() {
+      this.loading = true;
+      this.selected.file = -1;
+      this.files = [];
+      axios.post('/all', { type: 'entries', adapter: this.selected.adapter })
+        .then(response => {
+          this.files = response.data;
+        }).catch(err => {
+          this.error = err.message;
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    select(index) {
+      this.selected.file = index;
+    }
+  },
+  watch: {
+    filter() {
+      if (this.selected.file !== -1 &&
+          this.files[this.selected.file].name.indexOf(this.filter) === -1) {
+        this.selected.file = -1;
+      }
+    },
+    'selected.adapter': function() {
+      this.refresh();
+    }
+  },
+
+  mounted() {
+    // load adapters
+    axios.post('/all', { type: 'adapters' })
+      .then(response => {
+        this.adapters = response.data;
+      }).catch(err => {
+        this.error = err.message;
+      })
+      .then(() => {
+        this.loading = false;
+        this.$emit('ready');
+      });
+  }
+};
+</script>
