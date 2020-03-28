bdeb8f19ec36d02fc641777aeaf75a06bca81321
Axel Rindle <axel.rindle@gmx.de>
2020-03-28 17:44:26 +0100

    Export functions separately

--- backend/app/accounts.js
+++ backend/app/accounts.js
@@ -7,21 +7,41 @@ const clone = el => {
   return clone;
 };
 
+// Export utility functions
+
 /**
- * Export utility functions.
+ * An array of all accounts without sensitive data.
  */
-module.exports = {
-  all() {
-    return accounts.map(clone);
-  },
-  find(name) {
-    const account = accounts.find(el => el.name === name);
-    return account ? clone(account) : undefined;
-  },
-  has(name) {
-    return this.find(name) !== undefined;
-  },
-  checkAuth(name, pass) {
-    return this.has(name) && this.find(name).pass === pass;
-  }
+const all = module.exports.all = accounts.map(clone);
+
+/**
+ * Find the matching user account.
+ *
+ * @param {String} name The username to search.
+ * @returns {(Object|undefined)} the account object with the given name, or `null`.
+ */
+const find = module.exports.find = name => {
+  const account = all.find(el => el.name === name);
+  return account ? account : undefined;
 };
+
+/**
+ * Check whether an account with the given username exists.
+ *
+ * @param {String} name
+ * @returns {boolean}
+ */
+const has = module.exports.has = name => find(name) !== undefined;
+
+/**
+ * Check whether a given username/password combination is valid to authenticate.
+ *
+ * @param {string} name The username.
+ * @param {string} pass The password.
+ * @returns {boolean} Whether the authentication was successful.
+ */
+module.exports.checkAuth = (name, pass) => {
+  let success = has(name) && find(name).pass === pass;
+  console.log(`${name} + ${pass} => ${success}`);
+  return success;
+}
