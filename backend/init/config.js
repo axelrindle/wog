74f308e8b8c1624982c0e0efb90428b609ccfca1
Axel Rindle <axel.rindle@gmx.de>
2020-03-30 19:08:50 +0200

    Debug dotenv

--- backend/init/config.js
+++ backend/init/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const glob = require('glob-all');
 
 // Load .env file
-require('dotenv').config();
+require('dotenv').config({ debug: process.env.DEBUG });
 
 // Find config files
 const files = glob.sync(path.join(ROOT_DIRECTORY, 'config/*.js'));
