636499daafd403e10117d66c866e35db193980e3
Axel Rindle <axel.rindle@gmx.de>
2020-03-31 21:38:07 +0200

    Move to admin directory

---
+++ frontend/src/vue/admin/users/Editor.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div v-if="!loading">
+
+      <!-- Username-->
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Username</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control is-expanded has-icons-left">
+              <input class="input" type="text" :value="user.username" disabled="disabled" /><span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Role-->
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Role</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control is-expanded has-icons-left">
+              <div class="select">
+                <select v-model="user.role">
+                  <option value="admin">Admin</option>
+                  <option value="user">User</option>
+                </select>
+              </div><span class="icon is-small is-left"><i class="fas fa-tag"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Change password-->
+      <hr>
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Change password</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <div class="control is-expanded has-icons-left">
+              <input class="input" type="password" /><span class="icon is-small is-left"><i class="fas fa-key"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="buttons is-right">
+        <span class="button" @click="cancel">Cancel</span>
+        <span class="button is-success" @click="save">Save</span>
+      </div>
+    </div> <!-- end .card-content -->
+
+    <div class="loading" v-else>
+      <span style="margin-right: 10px;">Loading...</span>
+      <i class="fas fa-sync-alt fa-spin fa-lg"></i>
+    </div>
+
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'UserEditor',
+
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data() {
+    return {
+      loading: false,
+      showIndicator: {
+        refresh: false
+      }
+    }
+  },
+  computed: {
+    error() {
+      if (this.loading) return null;
+      if (!this.user) return 'No user selected!';
+      return null;
+    }
+  },
+
+  methods: {
+    cancel() {
+      this.$root.$refs.userList.selected = -1;
+    },
+    save() {
+      alert('User editing is currently not implemented.'); // TODO: Implement user editing
+    }
+  }
+};
+</script>
---
+++ frontend/src/vue/admin/users/List.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+
+    <!-- filter-->
+    <section class="section">
+      <div class="columns">
+
+        <!-- Search by username -->
+        <div class="column">
+          <label class="label">Search by Username</label>
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="johndoe" v-model="filter.username">
+            </div>
+            <div class="control">
+              <button class="button is-danger is-fullwidth" @click="filter = ''">
+                <span class="icon">
+                  <i class="fas fa-trash-alt"></i>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div> <!-- end .column -->
+
+        <!-- Filter by role -->
+        <div class="column">
+          <label class="label">Filter by Role</label>
+          <div class="select is-fullwidth">
+            <select v-model="filter.role">
+              <option value="all">All</option>
+              <option value="admin">Admins</option>
+              <option value="user">Users</option>
+            </select>
+          </div>
+        </div>
+
+        <!-- Infos -->
+        <div class="column">
+          <p>
+            Loaded {{ shown }} users
+          </p>
+          <p v-if="!allShown">
+            Showing {{ shown }} / {{ users.length }} users
+          </p>
+        </div>
+
+      </div> <!-- end .columns -->
+    </section> <!-- end .section -->
+
+    <hr>
+
+    <!-- file tree-->
+    <section class="section">
+      <div class="container">
+        <table class="table is-striped is-fullwidth is-hoverable">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user, index) in usersFiltered" :key="user.id" @click="selected = index">
+              <td>{{ user.id }}</td>
+              <td>{{ user.username }}</td>
+              <td>{{ user.role }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div> <!-- end .container -->
+    </section> <!-- end .section -->
+
+  </div> <!-- end wrapper -->
+</template>
+
+<script>
+module.exports = {
+  name: 'UserList',
+  data() {
+    return {
+      loading: true,
+      users: [],
+      filter: {
+        username: '',
+        role: 'all'
+      },
+      selected: -1
+    };
+  },
+  computed: {
+    usersFiltered() {
+      let list = this.users;
+
+      // filter by username
+      list = list.filter(el => el.username.indexOf(this.filter.username) !== -1);
+
+      // filter by role
+      if (this.filter.role !== 'all') {
+        list = list.filter(el => el.role.indexOf(this.filter.role) !== -1);
+      }
+
+      return list;
+    },
+    shown() {
+      return this.usersFiltered.length;
+    },
+    allShown() {
+      return this.shown === this.users.length;
+    },
+    theEntry() {
+      return this.users[this.selected];
+    }
+  },
+  methods: {
+    refresh() {
+      this.$root.error = null;
+      this.loading = true;
+      this.selected = -1;
+      this.users = [];
+      axios.post('/all/users')
+        .then(response => {
+          this.users = response.data;
+        }).catch(err => {
+          this.$root.error = err.message;
+        })
+        .then(() => {
+          this.loading = false;
+          this.$emit('ready');
+        });
+    }
+  },
+  watch: {
+    filter() {
+      if (this.selected !== -1 &&
+          this.users[this.selected].username.indexOf(this.filter) === -1) {
+        this.selected.file = -1;
+      }
+    },
+    'selected': function() {
+      localStorage.setItem('selectedUser', this.selected);
+    }
+  },
+
+  mounted() {
+    this.refresh();
+  }
+};
+</script>
