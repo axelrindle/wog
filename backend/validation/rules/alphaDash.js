798cd886784110d0ef9b3fbea0e281ca30c79cf2
Axel Rindle <axel.rindle@gmx.de>
2020-04-05 12:39:19 +0200

    Add info on length

--- backend/validation/rules/alphaDash.js
+++ backend/validation/rules/alphaDash.js
@@ -11,6 +11,6 @@ const ow = require('ow');
 module.exports = (minLength = 8) => {
   return ow.string.validate(value => ({
     validator: new RegExp(`^[a-zA-Z0-9]+[a-zA-Z0-9\\.\\-\\_]{${minLength - 2},}[a-zA-Z0-9]+$`, 'g').test(value),
-    message: label => `Expected ${label} to only contain alpha-numeric characters, dashes, underscores and dots!`
+    message: label => `Expected ${label} to have a minimum length of ${minLength} and to only contain alpha-numeric characters, dashes, underscores and dots!`
   }))
 };
