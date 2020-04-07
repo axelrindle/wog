64e3c96b8ea1c702dc0b049a5ead41da9fcc2431
Axel Rindle <axel.rindle@gmx.de>
2020-04-07 20:33:37 +0200

    Create dist.sh

---
+++ dist.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+archiveName=$(cat package.json | jq '.name,.version' -r | paste - - | xargs | sed "s/ /-/g" | sed "s/$/.tar.gz/")
+commitSha=$(git rev-parse HEAD)
+
+# Create archive from latest commit
+echo " >  Create git archive from commit $commitSha..."
+rm -rf dist/
+mkdir dist
+git archive --format=tar.gz "$commitSha" > "dist/$archiveName"
+
+# Create asset archive
+echo " >  Recompile assets..."
+rm -rf frontend/static/css/
+rm -rf frontend/static/js/
+npm run compile:scss
+npm run compile:js
+
+echo " >  Create asset archive..."
+tar -czvf dist/assets.tar.gz frontend/static/js/ frontend/static/css/
+
+echo " >  Done."
