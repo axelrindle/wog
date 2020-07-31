65e8068ba11400d046b8ec4084260ebce420d6e1
Axel Rindle <mail@axelrindle.de>
2020-07-31 20:03:11 +0200

    A BetterWebSocket is returned, not a WebSocket

--- frontend/src/js/util/BetterWebSocket.js
+++ frontend/src/js/util/BetterWebSocket.js
@@ -3,7 +3,7 @@ module.exports = class BetterWebSocket extends WebSocket {
    * Wrapper function for installing an event listener.
    * @param {string} event The event name.
    * @param {Function} callback The event callback.
-   * @return {WebSocket} This websocket to allow for chaining.
+   * @return {BetterWebSocket} This websocket to allow for chaining.
    */
   on(event, callback) {
     const theEvent = `on${event}`;
