b246e11437c94dc25ae03e2dfbcead34f246d51f
Axel Rindle <mail@axelrindle.de>
2020-08-10 20:58:18 +0200

    Disable the transition for initial state loading

--- frontend/src/js/sidebar.js
+++ frontend/src/js/sidebar.js
@@ -1,9 +1,11 @@
 const sidebar = $('#sidebar');
 
-// initial state
-sidebar.toggleClass('is-collapsed', localStorage.getItem('sidebar-state') === 'collapsed');
+sidebar
+  .addClass('is-transitionless')
+  .toggleClass('is-collapsed', localStorage.getItem('sidebar-state') === 'collapsed');
 
 $('#sidebar-collapse').click(() => {
+  if (sidebar.hasClass('is-transitionless')) sidebar.removeClass('is-transitionless');
   sidebar.toggleClass('is-collapsed');
   localStorage.setItem('sidebar-state', sidebar.hasClass('is-collapsed') ? 'collapsed' : 'expanded');
 });
