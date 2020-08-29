cd5911f70a7ec7815c1ac7b3fefef74fdcb38507
Axel Rindle <mail@axelrindle.de>
2020-08-29 21:23:12 +0200

    Allow .env file to be located in multiple locations

--- backend/init/config.js
+++ backend/init/config.js
@@ -1,9 +1,26 @@
 // Require modules
+const fs = require('fs');
 const path = require('path');
 const glob = require('glob-all');
 
+/**
+ * Finds a valid .env file out of valid locations.
+ */
+const envPath = () => {
+  const valid = [ '.env', 'storage/.env' ];
+  for (const el of valid) {
+    const joined = path.join(ROOT_DIRECTORY, el);
+    if (fs.existsSync(joined)) {
+      return joined;
+    }
+  }
+}
+
 // Load .env file
-require('dotenv').config({ debug: process.env.DEBUG });
+require('dotenv').config({
+  debug: process.env.DEBUG,
+  path: envPath()
+});
 
 // Find config files
 const files = glob.sync(path.join(ROOT_DIRECTORY, 'config/*.js'));
