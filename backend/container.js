4e28c77ae3a5e6738bbcafac18848df6b0a5cecf
Axel Rindle <mail@axelrindle.de>
2021-04-02 14:28:21 +0200

    Update register order, init afterwards

--- backend/container.js
+++ backend/container.js
@@ -12,32 +12,34 @@ const pkg = require('../package.json');
 const registerInitServices = async (container) => {
   container.register('events', awilix.asValue( new EventEmitter() ));
 
-  container.register('storage', awilix.asClass( require('./init/storage') ));
-  container.resolve('storage').createDirectory();
-  debug('Registered storage service.');
-
-  const config = await require('./init/config')(container);
+  const config = await require('./init/config')();
   container.register('config', awilix.asValue(config));
   debug('Registered static configuration.');
 
   container.register('logger', awilix.asFunction( require('./init/logger') ).singleton());
   debug('Registered the logger.');
 
+  container.register('storage', awilix.asClass( require('./init/storage') ));
+  debug('Registered storage service.');
+
   container.register('database', awilix.asClass( require('./init/database') ).singleton());
-  await container.resolve('database').init();
   debug('Registered database service.');
 
   container.register('accounts', awilix.asClass( require('./init/accounts') ).singleton());
-  await container.resolve('accounts').init();
   debug('Registered account service.');
 
   container.register('mailer', awilix.asClass( require('./init/mailer') ).singleton());
-  await container.resolve('mailer').init();
   debug('Registered mail service.');
 
   container.register('redis', awilix.asClass( require('./init/redis') ).singleton());
-  await container.resolve('redis').init();
   debug('Registered redis service.');
+
+  await container.resolve('storage').createDirectory();
+  await container.resolve('database').init();
+  await container.resolve('accounts').init();
+  await container.resolve('mailer').init();
+  await container.resolve('redis').init();
+  debug('Services initialized.');
 };
 
 /**
